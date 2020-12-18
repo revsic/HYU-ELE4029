082d556fbcb511507016e9ed3eb40fbb7a1544f9
revsic <revsic99@gmail.com>
2020-12-19 04:05:30 +0900

    analyze: Add type checking function

--- 3_Semantic/analyze.c
+++ 3_Semantic/analyze.c
@@ -51,40 +51,45 @@ static ScopeBlock scope[SCOPE_MAX];
 static int scopeidx;
 /* For empty name compound statement of new scoping */
 static int fnscope;
+/* For annonymous scope */
+static int annon_lineno;
+static int annon_num;
+
+static void init_scope_info(int startloc)
+{ scopeidx = 0;
+  fnscope = 0;
+  annon_lineno = 0;
+  annon_num = 0;
+  scope[0].name = "global";
+  scope[0].location = startloc;
+}
+
+static char * annon_scope_name(int lineno)
+{
+#define ANNON_PREFIX "annon_"
+#define ANNON_PREFIX_SIZE 6
+// maximum number of the digits.
+#define ANNON_POSTFIX 5
+  char * name = (char *)malloc(
+    sizeof(char) * (ANNON_PREFIX_SIZE + ANNON_POSTFIX + 1));
+  strcpy(name, ANNON_PREFIX);
+  if (annon_lineno != lineno)
+  { annon_lineno = lineno;
+    annon_num = 0;
+  }
+  snprintf(name + ANNON_PREFIX_SIZE, ANNON_POSTFIX, "%d", annon_num++);
+  return name;
+}
 
 /* Post process for insert node. */
 static void postInsert( TreeNode * t )
 { switch (t->nodekind)
-  { case DeclK:
-      switch (t->kind.decl)
-      { case ParamK:
-        case VarK:
-        case FnK:
-        default:
-          break;
-      }
-      break;
-    case StmtK:
+  { case StmtK:
       switch (t->kind.stmt)
       { case CompK:
           free(scope[scopeidx].name);
           scopeidx -= 1;
           break;
-        case IfK:
-        case WhileK:
-        case ReturnK:
-        default:
-          break;
-      }
-      break;
-    case ExpK:
-      switch (t->kind.exp)
-      { case AssignK:
-        case OpK:
-        case ConstK:
-        case IdK:
-        case CallK:
-        case IdxK:
         default:
           break;
       }
@@ -94,12 +99,19 @@ static void postInsert( TreeNode * t )
   }
 }
 
+static void typeError(TreeNode * t, char * message)
+{ fprintf(listing,"Error: %s at line %d (name : %s)\n",
+    message, t->lineno, t->attr.name);
+  Error = TRUE;
+}
+
 /* Procedure insertNode inserts 
  * identifiers stored in t into 
  * the symbol table 
  */
 static void insertNode( TreeNode * t)
-{ char * name;
+{ int size;
+  char * name;
   SymAddr addr;
   switch (t->nodekind)
   { case DeclK:
@@ -116,9 +128,17 @@ static void insertNode( TreeNode * t)
               t->attr.name, t->lineno);
           }
           else
+            size = -1;
+            if (t->child[0] != NULL)
+            { size = t->child[0]->attr.val;
+              if (size <= 0)
+              { typeError(t, "array size cannot be non-positive");
+                break;
+              }
+            }
             st_insert(
               scope_find(scope[scopeidx].name),
-              t->attr.name, t->type,
+              t->attr.name, t->type, size,
               t->lineno, scope[scopeidx].location++);
           break;
         case FnK:
@@ -131,7 +151,7 @@ static void insertNode( TreeNode * t)
           { // insert function
             addr.bucket = st_insert(
               scope_find(scope[scopeidx].name),
-              t->attr.name, Function,
+              t->attr.name, Function, -1,
               t->lineno, scope[scopeidx].location++);
             st_appendfn(addr.bucket, t);
             scope_insert(scope[scopeidx].name, t->attr.name);
@@ -152,15 +172,8 @@ static void insertNode( TreeNode * t)
           if (fnscope)
             fnscope = 0;
           else
-          { // generate annonymous scope with random postfix
-#define ANNON_PREFIX "annon_"
-#define ANNON_PREFIX_SIZE 6
-#define ANNON_POSTFIX 5
-            name = (char *)malloc(
-              sizeof(char) * (ANNON_PREFIX_SIZE + ANNON_POSTFIX + 1));
-            strcpy(name, ANNON_PREFIX);
-            randomFill(name + ANNON_PREFIX_SIZE, ANNON_POSTFIX);
-            name[ANNON_PREFIX_SIZE + ANNON_POSTFIX] = '\0';
+          { // generate annonymous scope with normalized postfix
+            name = annon_scope_name(t->lineno);
             // generate new scope
             scope_insert(scope[scopeidx].name, name);
             // update current scope
@@ -211,10 +224,7 @@ static void init_state()
 { // initialize scope
   int nextloc = global_init();
   // initialize scope block
-  scope[0].name = copyString("global");
-  scope[0].location = nextloc;
-  scopeidx = 0;
-  fnscope = 0;
+  init_scope_info(nextloc);
 }
 
 /* Function buildSymtab constructs the symbol 
@@ -235,67 +245,200 @@ void buildSymtab(TreeNode * syntaxTree)
   }
 }
 
-static void typeError(TreeNode * t, char * message)
-{ fprintf(listing,"Type error at line %d: %s\n",t->lineno,message);
-  Error = TRUE;
+static void scopeSetting(TreeNode * t)
+{ char * name;
+  switch (t->nodekind)
+  { case DeclK:
+      switch (t->kind.decl)
+      { case FnK:
+          // update current scope info
+          scopeidx += 1;
+          scope[scopeidx].name = copyString(t->attr.name);
+          scope[scopeidx].location = 0;
+          fnscope = 1;
+          break;
+        default:
+          break;
+      }
+      break;
+    case StmtK:
+      switch (t->kind.stmt)
+      { case CompK:
+          if (fnscope)
+            fnscope = 0;
+          else
+          { // update current scope
+            scopeidx += 1;
+            scope[scopeidx].name = annon_scope_name(lineno);;
+            scope[scopeidx].location = 0;
+          }
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
 }
 
 /* Procedure checkNode performs
  * type checking at a single tree node
  */
 static void checkNode(TreeNode * t)
-{ 
-  // switch (t->nodekind)
-  // { case ExpK:
-  //     switch (t->kind.exp)
-  //     { case OpK:
-  //         if ((t->child[0]->type != Integer) ||
-  //             (t->child[1]->type != Integer))
-  //           typeError(t,"Op applied to non-integer");
-  //         if ((t->attr.op == EQ) || (t->attr.op == LT))
-  //           t->type = Boolean;
-  //         else
-  //           t->type = Integer;
-  //         break;
-  //       case ConstK:
-  //       case IdK:
-  //         t->type = Integer;
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //     break;
-  //   case StmtK:
-  //     switch (t->kind.stmt)
-  //     { case IfK:
-  //         if (t->child[0]->type == Integer)
-  //           typeError(t->child[0],"if test is not Boolean");
-  //         break;
-  //       case AssignK:
-  //         if (t->child[0]->type != Integer)
-  //           typeError(t->child[0],"assignment of non-integer value");
-  //         break;
-  //       case WriteK:
-  //         if (t->child[0]->type != Integer)
-  //           typeError(t->child[0],"write of non-integer value");
-  //         break;
-  //       case RepeatK:
-  //         if (t->child[1]->type == Integer)
-  //           typeError(t->child[1],"repeat test is not Boolean");
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //     break;
-  //   default:
-  //     break;
+{ int i;
+  TreeNode * node;
+  SymAddr addr;
+  switch (t->nodekind)
+  { case DeclK:
+      switch (t->kind.decl)
+      { case ParamK:
+          // only single unnamed void parameter is available
+          if (t->type == Void && (
+            // (null) is predefined empty void parameter name
+              strcmp(t->attr.name, "(null)") || t->sibling != NULL))
+            typeError(t, "Variable Type cannot be Void");
+          break;
+        case VarK:
+          // void cannot be variable type
+          if (t->type == Void)
+            typeError(t, "Variable Type cannot be Void");
+          break;
+        case FnK:
+          // Do Nothing
+          break;
+        default:
+          break;
+      }
+      break;
+    case ExpK:
+      switch (t->kind.exp)
+      { case AssignK:
+          // only expression to variable is assignable
+          if (t->child[0]->nodekind != ExpK || t->child[0]->kind.exp != IdK
+              || t->child[1]->nodekind != ExpK)
+          { typeError(t, "invalid expression");
+            break;
+          }
+          // cannot assign to function
+          if (t->child[0]->type == Function)
+          { typeError(t, "cannot assign to function");
+            break;
+          }
+          if (t->child[0]->type == Array)
+          { typeError(t, "cannot assign to array variable");
+            break;
+          }
+          // only expression of same type is assignable
+          if (t->child[0]->type != t->child[1]->type)
+          { typeError(t, "type miss match");
+            break;
+          }
+          // assign type
+          t->type = t->child[0]->type;
+          break;
+        case OpK:
+          // currently only integer operation is available
+          if ((t->child[0]->type != Integer) ||
+              (t->child[1]->type != Integer))
+            typeError(t,"operation applied to non-integer");
+          // assume all operation results are integer, no boolean
+          t->type = Integer;
+          break;
+        case ConstK:
+          // Do Nothing
+          break;
+        case IdK:
+          addr = st_lookup(scope[scopeidx].name, t->attr.name);
+          if (addr.bucket->size > 0 && t->child[0] == NULL)
+            // array declared but do not have indexing child
+            t->type = Array;
+          else
+            t->type = addr.bucket->type;
+          break;
+        case CallK:
+          addr = st_lookup(scope[scopeidx].name, t->attr.name);
+          // counting number of the arguments
+          i = 0;
+          node = t->child[0];
+          while (node != NULL)
+          { i += 1;
+            node = node->sibling;
+          }
+          // check parameter numbers
+          if (i != addr.bucket->fninfo->numparam)
+          { typeError(t, "the numbers of the parameters are different");
+            break;
+          }
+          // check param type
+          node = t->child[0];
+          for (i = 0; i < addr.bucket->fninfo->numparam; ++i)
+          { if (addr.bucket->fninfo->params[i].type != node->type)
+            { typeError(t, "parameter type mismatch");
+              break;
+            }
+            node = node->sibling;
+          }
+          // assign type
+          t->type = addr.bucket->fninfo->retn;
+          break;
+        case IdxK:
+          if (t->child[0]->type != Integer)
+            typeError(t, "index should be integer");
+            break;
+        default:
+          break;
+      }
+      break;
+    case StmtK:
+      switch (t->kind.stmt)
+      { case CompK:
+          // for scope post processing
+          free(scope[scopeidx].name);
+          scopeidx -= 1;
+          break;
+        case IfK:
+          // only integer condition is available
+          // on parsing level, empty condition is filtered
+          if (t->child[0]->type != Integer)
+            typeError(t->child[0],"if test is not integer");
+          break;
+        case WhileK:
+          // only integer condition is available
+          // on parsing level, empty condition is filtered
+          if (t->child[0]->type != Integer)
+            typeError(t->child[0],"while test is not integer");
+          break;
+        case ReturnK:
+          // scopidx == 0 -> global scope
+          // return can only be in the function scope
+          if (scopeidx == 0)
+          { typeError(t, "return cannot be occured in global");
+            break;
+          }
+          // then scopeidx >= 1 and scope[1] is method scope,
+          // since function can be only declared on global scope by parser.
+          addr = st_lookup("global", scope[1].name);
+          if (t->child[0] == NULL)
+            if (addr.bucket->fninfo->retn != Void)
+              typeError(t, "return nothing on non-void function");
+          // child[0] is not null
+          else if (t->child[0]->type != addr.bucket->fninfo->retn)
+            typeError(t, "return type mismatch");
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
 
-  // }
+  }
 }
 
 /* Procedure typeCheck performs type checking 
  * by a postorder syntax tree traversal
  */
 void typeCheck(TreeNode * syntaxTree)
-{ traverse(syntaxTree,nullProc,checkNode);
+{ traverse(syntaxTree,scopeSetting,checkNode);
 }
