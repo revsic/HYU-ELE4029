ef4c34f7bd2a3252fbbd2bf3c653c05e37310bf7
revsic <revsic99@gmail.com>
2020-12-18 17:07:19 +0900

    symtab: Add additional functions

--- 3_Semantic/symtab.h
+++ 3_Semantic/symtab.h
@@ -9,17 +9,24 @@
 #ifndef _SYMTAB_H_
 #define _SYMTAB_H_
 
+#include "globals.h"
+
 /* Procedure st_insert inserts line numbers and
  * memory locations into the symbol table
  * loc = memory location is inserted only the
  * first time, otherwise ignored
  */
-void st_insert( char * name, int lineno, int loc );
+void st_insert( char * scope, char * name, ExpType type, int lineno, int loc );
 
 /* Function st_lookup returns the memory 
  * location of a variable or -1 if not found
  */
-int st_lookup ( char * name );
+BucketList st_lookup ( char * scope, char * name );
+
+/* Function st_lookup_excluding_parent returns the memory
+ * location of variable or -1 if not found only in the specified scope (excluding parent).
+ */
+BucketList st_lookup_excluding_parent ( char * scope, char * name );
 
 /* Procedure printSymTab prints a formatted 
  * listing of the symbol table contents 
